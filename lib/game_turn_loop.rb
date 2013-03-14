6c411d603c8b7be39a05ebc320710fc8f65125f4
David Pardy <dpardy87@gmail.com>
2013-03-13 21:15:54 -0500

    added separate arrays for player 1 and 2

--- lib/game_turn_loop.rb
+++ lib/game_turn_loop.rb
@@ -5,12 +5,27 @@ class GameTurnLoop
     symbol = 'X'
       while !game_over
       #9.times do # don't want this to be 9, you want it to loop until game is over-try while loop
-        selection = player.move
-        board.spaces[selection] = symbol
+        selection1 = player.move
+        board.spaces[selection1] = symbol
+        p1 ||= []
+        p1 << selection1
+        print p1
+        puts "\n"
+        symbol = switch_symbols(symbol)
+        board.draw_board unless winning_combos
+
+        selection2 = player.move
+        board.spaces[selection2] = symbol
+        p2 ||= []
+        p2 << selection2
+        print p2
+        puts "\n"
         symbol = switch_symbols(symbol)
         board.draw_board
       end
   end
+
+  
 
   def self.switch_symbols(symbol)
     if symbol == 'X'
@@ -21,3 +36,4 @@ class GameTurnLoop
   end
 
 end
+
