ed4d0a35a9f485d43bf5ab6965063239638655a4
David Pardy <dpardy87@gmail.com>
2013-03-13 22:32:30 -0500

    moved wc over to gtl

--- lib/board.rb
+++ lib/board.rb
@@ -3,9 +3,9 @@ class Board
 
   def initialize
     @spaces = { 1 => "1", 2 => "2", 3 => "3", 4 => "4", 5 => "5", 6 => "6", 7 => "7", 8 => "8", 9 => "9" }
-    @p1
-    @p2
-    @game_over
+    #@p1
+    #@p2
+    
   end
 
   def draw_board
@@ -19,17 +19,21 @@ class Board
     -----------"
   end
 
-  def winning_combos
-    columns = [1, 4, 7] || [2, 5, 8] || [3, 6, 9]
-    rows = [1, 2, 3] || [4, 5, 6] || [7, 8, 9]
-    diagnols = [1, 5, 9] || [3, 5, 7]
+  # def winning_combo_found
+  #   columns = [1, 4, 7] || [2, 5, 8] || [3, 6, 9]
+  #   rows = [1, 2, 3] || [4, 5, 6] || [7, 8, 9]
+  #   diagnols = [1, 5, 9] || [3, 5, 7]
+  #   #winning combos should just return true or false
+  #   if columns == true
+  #     game_ova
+  #   end
 
-    if @p1 || @p2 = columns || rows || diagnols
-      @game_over
-    else
-      @game_over = false
-    end
-  end 
+  #   if @p1 || @p2 = columns || rows || diagnols
+  #     @game_over
+  #   else
+  #     @game_over = false
+  #   end
+  # end 
 
   # def computer_move
   #   if @spaces[5] == "5"
