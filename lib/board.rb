0010cd068abafcc32e8a45cccd81797061813fca
David Pardy <dpardy87@gmail.com>
2013-03-15 14:03:40 -0500

    just trying one winning combo for now, being recognized but has to be correct order

--- lib/board.rb
+++ lib/board.rb
@@ -21,7 +21,7 @@ class Board
 
   def winning_combo_found
     
-    [1, 4, 7] || [2, 5, 8] || [3, 6, 9] || [1, 2, 3] || [4, 5, 6] || [7, 8, 9] || [1, 5, 9] || [3, 5, 7]
+    [1, 4, 7] #|| [2, 5, 8] || [3, 6, 9] || [1, 2, 3] || [4, 5, 6] || [7, 8, 9] || [1, 5, 9] || [3, 5, 7]
     #winning combos should just return true or false
     #if columns == true
      # "FUCK FUCK FUCK"
