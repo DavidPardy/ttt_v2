d52984b9c50b02cac866869425ba22a646e74a57
David Pardy <dpardy87@gmail.com>
2013-03-15 16:36:25 -0500

    added Computer.new to start game

--- lib/start_game.rb
+++ lib/start_game.rb
@@ -3,7 +3,9 @@ $LOAD_PATH.unshift(File.dirname(__FILE__))
 require 'game_turn_loop'
 require 'board'
 require 'player'
+require 'computer'
 
 board = Board.new
 player = Player.new
-GameTurnLoop.play(board, player)
+computer = Computer.new
+GameTurnLoop.play(board, player, computer)
