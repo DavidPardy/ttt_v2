718b1380345dcf984c57fb028101d41b0c71ac34
David Pardy <dpardy87@gmail.com>
2013-03-15 14:06:28 -0500

    one combo working correctly

--- lib/game_turn_loop.rb
+++ lib/game_turn_loop.rb
@@ -7,18 +7,18 @@ class GameTurnLoop
       #9.times do # don't want this to be 9, you want it to loop until game is over-try while loop
         selection1 = player.move
         board.spaces[selection1] = symbol
-        p1 ||= []
-        p1 << selection1
-        print p1
+        @p1 ||= []
+        @p1 << selection1
+        print @p1
         puts "\n"
         symbol = switch_symbols(symbol)
         board.draw_board unless game_ova(board) #board.winning_combo_found
 
         selection2 = player.move
         board.spaces[selection2] = symbol
-        p2 ||= []
-        p2 << selection2
-        print p2
+        @p2 ||= []
+        @p2 << selection2
+        print @p2
         puts "\n"
         symbol = switch_symbols(symbol)
         board.draw_board unless game_ova(board)
@@ -34,7 +34,7 @@ class GameTurnLoop
   end
   
   def self.game_ova(board)
-    if board.winning_combo_found.any?
+    if board.winning_combo_found == @p1
       puts "the game is up bro"
     else
       puts "keep going"
