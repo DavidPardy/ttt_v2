9703fa449bdd66cf724d45d0b927e64a8d2952b5
David Pardy <dpardy87@gmail.com>
2013-03-15 18:11:05 -0500

    setup move and random move methods, added empty methods for optimal winning and blocking

--- lib/computer.rb
+++ lib/computer.rb
@@ -1,5 +1,30 @@
 class Computer
 	def random_move(board, space)
 		spaces[space] = 'O'
+		puts "Tile #{space} was chosen by the computer player."
 	end
+
+	def move(board, player, computer)
+		board.spaces.each do |spot, x_or_o|
+			if x_or_o != 'X' && x_or_o != 'O'
+				puts "It's the computer's turn..."
+				sleep(1)
+				random_move(board, space)
+				break
+			end
+		end
+	end
+
+	def optimal_move
+
+	end
+
+	def winning_move
+		
+	end
+
+	def blocking_move
+		
+	end
+
 end
