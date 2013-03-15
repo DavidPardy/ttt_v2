79fff3b5f2ff1f19066ed23e4f1bc73d3895c3fc
David Pardy <dpardy87@gmail.com>
2013-03-15 18:57:55 -0500

    changed some names

--- lib/game_turn_loop.rb
+++ lib/game_turn_loop.rb
@@ -7,15 +7,15 @@ class GameTurnLoop
 
     board.draw_board
     while !@game_over
-      selection1 = player.move
-      board.spaces[selection1] = get_symbol
-      @p1 << selection1
+      player_move = player.move
+      board.spaces[player_move] = get_symbol
+      @p1 << player_move
       puts "#{@p1} \n"
       board.draw_board unless game_ova(board)
 
-      selection2 = computer.move(board, @p1, @p2)
-      board.spaces[selection2] = get_symbol
-      @p2 << selection2
+      computer_move = computer.move(board, @p1, @p2)
+      board.spaces[computer_move] = get_symbol
+      @p2 << computer_move
       puts "#{@p2} \n"
       board.draw_board unless game_ova(board)
     end
@@ -33,7 +33,7 @@ class GameTurnLoop
       @game_over = true
     elsif board.winning_combo_found(@p1, @p2) == "computer_winner"
       puts "computer player wins"
-      @game_over
+      @game_over = true
     else
       puts "no one wins"
     end
