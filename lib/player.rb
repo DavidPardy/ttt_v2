e61130a934db6336a8ccb7dd8a21a12790870f00
David Pardy <dpardy87@gmail.com>
2013-03-16 10:11:14 -0500

    removed whitespace

--- lib/player.rb
+++ lib/player.rb
@@ -1,5 +1,11 @@
 class Player
   def move
-    gets.to_i
+  	puts "Your turn, human!"
+    input = gets.to_i
+    if input.between?(1, 9)
+    	puts "You have selectted spot #{input}"
+    else
+    	puts "wrong input broski" if !input.between(1, 9)
+    end
   end
 end
