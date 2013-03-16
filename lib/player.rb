28fe5939fae8c8bf083ce987e05c1455e9a1e6eb
David Pardy <dpardy87@gmail.com>
2013-03-16 10:54:42 -0500

    keep move method simple for now

--- lib/player.rb
+++ lib/player.rb
@@ -1,11 +1,5 @@
 class Player
   def move
-  	puts "Your turn, human!"
-    input = gets.to_i
-    if input.between?(1, 9)
-    	puts "You have selectted spot #{input}"
-    else
-    	puts "wrong input broski" if !input.between(1, 9)
-    end
+  	gets.to_i
   end
 end
