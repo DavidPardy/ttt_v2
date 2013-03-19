2ea91148c82068154495df31088c785426f7a814
David Pardy <dpardy87@gmail.com>
2013-03-18 21:03:48 -0500

    changed string again

--- lib/computer.rb
+++ lib/computer.rb
@@ -1,61 +1,61 @@
 class Computer
-	def move(board, player, computer)
-		if optimal_move(board, player, computer) == false
-			board.spaces.each do |space, symbol|
-				if symbol != 'X' && symbol != 'O'
-					puts "It's the computer's turn..."
-					sleep(1)
-					random_move(board, space)
-					break
-				end
-			end
-		end
-	end
+  def move(board, player, computer)
+    if optimal_move(board, player, computer) == false
+      board.spaces.each do |space, symbol|
+        if symbol != 'X' && symbol != 'O'
+          puts "It's the computer's turn..."
+          sleep(1)
+          random_move(board, space)
+          break
+        end
+      end
+    end
+  end
 
-	def random_move(board, space)
-		board.spaces[space] = 'O'
-		puts "Tile #{space} was chosen by the computer."
-	end
+  def random_move(board, space)
+    board.spaces[space] = 'O'
+    puts "Tile #{space} was chosen by the computer player."
+  end
 
-	def optimal_move(board, player, computer)
-		winning = winning_move(board, computer)
-		optimal = winning
+  def optimal_move(board, player, computer)
+    winning = winning_move(board, computer)
+    optimal = winning
 
-		blocking = blocking_move(board, player)
-		optimal ||= blocking
+    blocking = blocking_move(board, player)
+    optimal ||= blocking
 
-		if optimal != false
-			random_move(board, optimal)
-		else
-			return false
-		end
-	end
+    if optimal != false
+      random_move(board, optimal)
+    else
+      return false
+    end
+  end
 
-	def winning_move(board, computer)
-		Board::WINNING_COMBOS.each do |combo|
-			difference = combo - computer
-			if difference.length == 1
-				move = difference.first
-				if board.available_space?(move)
-					return move
-				end
-			end
-		end
-		return false
-	end
+  def winning_move(board, computer)
+    Board::WINNING_COMBOS.each do |combo|
+      difference = combo - computer
+      if difference.length == 1
+        move = difference.first
+        if board.available_space?(move)
+          return move
+        end
+      end
+    end
+    return false
+  end
 
-	def blocking_move(board, player)
-		Board::WINNING_COMBOS.each do |combo|
-			difference = combo - player
-			if difference.length == 1
-				move = difference.first
-				if board.available_space?(move)
-					sleep(1)
-					return move
-				end
-			end
-		end
-		return false
-	end
+  def blocking_move(board, player)
+    Board::WINNING_COMBOS.each do |combo|
+      difference = combo - player
+      if difference.length == 1
+        move = difference.first
+        if board.available_space?(move)
+          sleep(1)
+          return move
+        end
+      end
+    end
+    return false
+  end
 
 end
