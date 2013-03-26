d9f50dd4329cc18da9b882e780af7cef8d5572a7
David Pardy <dpardy87@gmail.com>
2013-03-25 23:40:15 -0500

    used array intersect method to find winning combos correctly

--- lib/board.rb
+++ lib/board.rb
@@ -2,7 +2,7 @@ class Board
   attr_reader :spaces
 
   WINNING_COMBOS = [[1, 4, 7], [2, 5, 8], [3, 6, 9], [1, 2, 3], [4, 5, 6], [7, 8, 9], [1, 5, 9], [3, 5, 7]]
-  NON_WINNING_COMBOS = [[1, 6], [1, 8], [2, 4], [2, 6], [2, 7], [2, 9], [3, 4], [3, 8], [4, 8], [4, 9], [6, 7], [6, 8]]
+
   def initialize
     @spaces = { 1 => "1", 2 => "2", 3 => "3", 4 => "4", 5 => "5", 6 => "6", 7 => "7", 8 => "8", 9 => "9" }
   end
@@ -19,19 +19,14 @@ class Board
   end
 
   def winning_combo_found(human, computer)
-    human_winner = WINNING_COMBOS.select { |combo| combo == human}.any?
-    computer_winner = WINNING_COMBOS.select { |combo| combo == computer}.any?
-    return "human_winner" if human_winner
-    return "computer_winner" if computer_winner
+    human_winner = WINNING_COMBOS.select { |combo| (human & combo).sort == combo }
+    computer_winner = WINNING_COMBOS.select { |combo| (computer & combo).sort == combo }
+    return "human_winner" if human_winner.any?
+    return "computer_winner" if computer_winner.any
   end
 
   def available_space?(space)
-    #return true if @spaces[space].nil? #== space.to_s
-    @spaces[space].nil?
-  end
-
-  def get_empty_spaces
-   (1..9).select{|space| @spaces[space].nil?}
+    @spaces[space] != 'X' && @spaces[space] != 'O'
   end
 
 end
