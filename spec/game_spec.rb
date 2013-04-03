f4bcfcbda644542ba2b7013c16cbabe67c38fb03
David Pardy <dpardy87@gmail.com>
2013-04-03 15:54:48 -0500

    working

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,17 +1,15 @@
-
-require 'board'
 require 'computer'
+require 'board'
+
 
 describe "selects winning move" do
   it "the computer selects spot 3 when spot 3 is a winning move for computer" do
     @p1 = []
     @p2 = [1, 2]
-    board = Board.new
     computer = Computer.new
-    board.spaces[1] = "O"
-    board.spaces[2] = "O"
-    computer_move = computer.winning_move(board, [1, 2])
-    computer.winning_move(board, [1,2]).should eq(computer_move)
+    board = Board.new
+  
+    computer.winning_move(board, [1,2]).should eq(3)
   end
 end
      
