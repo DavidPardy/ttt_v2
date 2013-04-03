d83fbe9ea1766d7e7964e3382ca6fc5702219bce
David Pardy <dpardy87@gmail.com>
2013-04-03 14:22:18 -0500

    little progress

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,12 +1,18 @@
 require 'game'
-require 'rspec'
+require 'board'
+require 'computer'
 
 describe "selects winning move" do
   it "the computer selects spot 3 when spot 3 is a winning move for computer" do
+    @p1 = []
+    @p2 = []
     board = Board.new
     computer = Computer.new
     computer_move = computer.move(board, @p1, @p2)
-    board.spaces[computer_move] = ['O', 'O', 3]
+    
+    board.spaces[computer_move] = 'O'
+    board.draw_board
+
     board.spaces[computer_move].should == ['O', 'O', 'O']
   end
 end
