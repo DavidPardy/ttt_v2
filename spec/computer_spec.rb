f07a1dc08c79419bbc23d979c37151ba4f3af564
David Pardy <dpardy87@gmail.com>
2013-04-04 10:48:55 -0500

    refactored to remove @p1 and @p2 bc its unnecessary

--- spec/computer_spec.rb
+++ spec/computer_spec.rb
@@ -1,11 +1,8 @@
 require 'computer'
 require 'board'
 
-
 describe "selects winning move" do
   it "the computer selects spot 3 when spot 3 is a winning move for computer" do
-    @p1 = []
-    @p2 = [1, 2]
     computer = Computer.new
     board = Board.new
   
