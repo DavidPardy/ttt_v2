c66a4c6a93666769fb88921e092c8fe0d22e6c9e
David Pardy <dpardy87@gmail.com>
2013-04-04 10:43:38 -0500

    changed name from game_spec to computer_spec

---
+++ spec/computer_spec.rb
@@ -0,0 +1,14 @@
+require 'computer'
+require 'board'
+
+
+describe "selects winning move" do
+  it "the computer selects spot 3 when spot 3 is a winning move for computer" do
+    @p1 = []
+    @p2 = [1, 2]
+    computer = Computer.new
+    board = Board.new
+  
+    computer.winning_move(board, [1,2]).should eq(3)
+  end
+end
