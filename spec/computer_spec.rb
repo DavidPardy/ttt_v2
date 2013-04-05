d2c1014178111f0f408de069de15871a0e37027d
David Pardy <dpardy87@gmail.com>
2013-04-04 20:45:51 -0500

    testing blocking move method, working correctly

--- spec/computer_spec.rb
+++ spec/computer_spec.rb
@@ -1,7 +1,16 @@
 describe "selects winning move" do
-  it "the computer selects spot 3 when spot 3 is a winning move for computer" do
+  it "the computer selects the correct spot when it has an available winning move" do
     computer = Computer.new
     board = Board.new
     computer.winning_move(board, [1,2]).should eq(3)
   end
 end
+
+describe "selects blocking move" do
+  it "the computer selects the correct spot when the human has a winning move" do
+    computer = Computer.new
+    board = Board.new
+    computer.blocking_move(board, [3, 9]).should eq(6)
+  end
+end
+
