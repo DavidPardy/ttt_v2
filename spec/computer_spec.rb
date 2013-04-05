35f448f5dee93b6289e2e7211f13c1c6d45ce035
David Pardy <dpardy87@gmail.com>
2013-04-04 23:31:34 -0500

    started 3rd test

--- spec/computer_spec.rb
+++ spec/computer_spec.rb
@@ -14,3 +14,10 @@ describe "the computer player" do
   end
 end
 
+describe "the computer player" do
+  it "selects a winning move over a blocking move (optimal move)" do
+    computer = Computer.new
+    board = Board.new
+    computer.optimal_move(board, [])
+  end
+end
