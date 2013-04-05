9b1c7df9addb2fbe9c4f6e2da3c769f0847c42a1
David Pardy <dpardy87@gmail.com>
2013-04-04 20:47:01 -0500

    modified describe and it

--- spec/computer_spec.rb
+++ spec/computer_spec.rb
@@ -1,13 +1,13 @@
-describe "selects winning move" do
-  it "the computer selects the correct spot when it has an available winning move" do
+describe "the computer player" do
+  it "selects the correct spot when it has an available winning move" do
     computer = Computer.new
     board = Board.new
     computer.winning_move(board, [1,2]).should eq(3)
   end
 end
 
-describe "selects blocking move" do
-  it "the computer selects the correct spot when the human has a winning move" do
+describe "the computer player" do
+  it "selects the correct spot when the human has a winning move" do
     computer = Computer.new
     board = Board.new
     computer.blocking_move(board, [3, 9]).should eq(6)
