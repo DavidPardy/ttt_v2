56f611915b5c319743e311bec909531b77a31136
David Pardy <dpardy87@gmail.com>
2013-04-04 19:08:15 -0500

    using spec_helper now

--- spec/computer_spec.rb
+++ spec/computer_spec.rb
@@ -1,11 +1,9 @@
-require 'computer'
-require 'board'
+require 'spec_helper'
 
 describe "selects winning move" do
   it "the computer selects spot 3 when spot 3 is a winning move for computer" do
     computer = Computer.new
     board = Board.new
-  
     computer.winning_move(board, [1,2]).should eq(3)
   end
 end
