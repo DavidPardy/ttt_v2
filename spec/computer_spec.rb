d64bb81fada22e0e69a132845d6aa967bc4209d8
David Pardy <dpardy87@gmail.com>
2013-04-24 18:18:28 -0500

    added new test for specific files

--- spec/computer_spec.rb
+++ spec/computer_spec.rb
@@ -4,20 +4,23 @@ describe "the computer player" do
     board = Board.new
     computer.winning_move(board, [1,2]).should eq(3)
   end
-end
 
-describe "the computer player" do
   it "selects the correct spot when the human has a winning move" do
     computer = Computer.new
     board = Board.new
     computer.blocking_move(board, [3, 9]).should eq(6)
   end
-end
 
-describe "the computer player" do
-  it "selects a winning move over a blocking move (optimal move)" do
+  # it "selects a winning move over a blocking move (optimal move)" do
+  #   computer = Computer.new
+  #   board = Board.new
+  #   computer.optimal_move(board, [])
+  # end
+
+  it "selects a competitive move when the human has spots 4 and 8" do
     computer = Computer.new
     board = Board.new
-    computer.optimal_move(board, [])
+    computer.competitive_move(board, [5]).should eq(6)
   end
+  #is human going to be included in the comp move method param?
 end
