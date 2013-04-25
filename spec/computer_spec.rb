4166eca28d7b36b885dd426ad992b6f934abade0
David Pardy <dpardy87@gmail.com>
2013-04-25 12:39:43 -0500

    added test for if human selects 3 than 8

--- spec/computer_spec.rb
+++ spec/computer_spec.rb
@@ -17,10 +17,22 @@ describe "the computer player" do
   #   computer.optimal_move(board, [])
   # end
 
-  it "selects a competitive move when the human has spots 4 and 8" do
-    computer = Computer.new
+  # it "selects a competitive move when the human has spots 4 and 8" do
+  #   computer = Computer.new
+  #   board = Board.new
+  #   computer.competitive_move(board, [5]).should eq(6)
+  # end
+  
+  it "selects a competitive move when 3 and 8 are selected" do
     board = Board.new
-    computer.competitive_move(board, [5]).should eq(6)
+    board.spaces = { 1 => "1", 2 => "2", 3 => "X", 4 => "4", 5 => "O", 6 => "6", 7 => "7", 8 => "X", 9 => "9" }
+    computer = [5]
+    human = [3,8]
+
+    move = Computer.new.competitive_move(board, human, computer)
+
+    puts "***MOVE IS #{move}***"
+    move.should_not == 2
   end
-  #is human going to be included in the comp move method param?
+  
 end
