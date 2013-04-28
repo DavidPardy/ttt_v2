f7ccce3654dcea856c8c67dc3cf872e7f645a82b
David Pardy <dpardy87@gmail.com>
2013-04-28 16:56:32 -0500

    added simplecov gem

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,3 +3,10 @@ require 'board'
 require 'game'
 require 'computer'
 require 'human'
+
+begin
+  require 'simplecov'
+  SimpleCov.start
+rescue LoadError
+  puts 'Coverage disabled, enable by installing simplecov'
+end
